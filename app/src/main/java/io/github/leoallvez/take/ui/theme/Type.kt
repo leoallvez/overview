7d18ce0bc43e8dbcaa7b7ccf0e700f24673f4aea
leoallvez <leoallvez@gmail.com>
2021-12-30 18:55:30 -0300

    configure logo font

--- app/src/main/java/io/github/leoallvez/take/ui/theme/Type.kt
+++ app/src/main/java/io/github/leoallvez/take/ui/theme/Type.kt
@@ -2,14 +2,24 @@ package io.github.leoallvez.take.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import io.github.leoallvez.take.R
 
+val MrDafoe = FontFamily(
+    Font(R.font.mr_dafoe)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    overline = TextStyle(
+        fontFamily = MrDafoe,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
