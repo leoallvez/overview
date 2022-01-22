b234c1e15828047ed4c64e7bfc3631d0520053d9
leoallvez <leoallvez@gmail.com>
2022-01-22 02:33:33 -0300

    create data base classes

---
+++ app/src/main/java/io/github/leoallvez/take/di/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package io.github.leoallvez.take.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.github.leoallvez.take.db.MovieDao
+import io.github.leoallvez.take.db.TakeDatabase
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): TakeDatabase {
+        return Room.databaseBuilder(
+            context,
+            TakeDatabase::class.java,
+            "take_database"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTaskDao(db: TakeDatabase): MovieDao {
+        return db.movieDao()
+    }
+}
+
