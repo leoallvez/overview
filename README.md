21576267c7a880eb296e8f883e57a2d612b1f8a0
leoallvez <leoallvez@gmail.com>
2022-03-26 02:48:49 -0300

    basic of splash screen

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 Android application to know where to watch a movie or series on streaming services.
 
+##Configurations
+It is crucial for the application to work the creation of the 'api.properties' in the project root,
+this file should to contain **The Movie Database API** values:
+
+```
+API_KEY = "Your The movie Database API KEY"
+API_URL = "https://api.themoviedb.org/3/"
+URL_IMG = "https://image.tmdb.org/t/p/w500/"
+```
+
 learn more about the development process [here](https://tree.taiga.io/project/leoallvez-take/timeline).
--- app/src/main/java/io/github/leoallvez/take/MainActivity.kt
+++ app/src/main/java/io/github/leoallvez/take/MainActivity.kt
@@ -1,33 +1,21 @@
 package io.github.leoallvez.take
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.leoallvez.firebase.RemoteConfigKey.*
-import io.github.leoallvez.firebase.RemoteSource
-import io.github.leoallvez.take.abtest.ExampleStrategy
+import io.github.leoallvez.take.ui.splash.SplashScreen
+import io.github.leoallvez.take.ui.main.HomeScreen
 import io.github.leoallvez.take.ui.theme.TakeTheme
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var remote: RemoteSource
-    @Inject
-    lateinit var example: ExampleStrategy
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,36 +23,25 @@ class MainActivity : ComponentActivity() {
             TakeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Logo()
+                    TakeApp()
                 }
             }
         }
-        //val name = remote.getString(key = NAME_KEY)
-        Log.i("my_model", "model before: ${example.valueToChange}")
-        example.execute()
-        Log.i("my_model", "model after : ${example.valueToChange}")
     }
 }
 
 @Composable
-fun Logo() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+fun TakeApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
     ) {
-        Text(
-            text = "Take",
-            style = MaterialTheme.typography.overline,
-            fontSize = 100.sp,
-            //modifier = Modifier.padding(10.dp)
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TakeTheme {
-        Logo()
+        composable(route = Screen.Splash.route) {
+            SplashScreen(navController)
+        }
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
     }
 }
