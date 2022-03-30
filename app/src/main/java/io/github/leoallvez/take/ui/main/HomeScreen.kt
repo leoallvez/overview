74414691157403666a15df9605c4a34da9e4b310
leoallvez <leoallvez@gmail.com>
2022-03-29 23:30:17 -0300

    home basic layout

--- app/src/main/java/io/github/leoallvez/take/ui/main/HomeScreen.kt
+++ app/src/main/java/io/github/leoallvez/take/ui/main/HomeScreen.kt
@@ -1,22 +1,96 @@
 package io.github.leoallvez.take.ui.main
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Preview
 @Composable
 fun HomeScreen() {
+    val movies: List<String> = (0..99).map { "movie $it" }
     Box(
-        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.DarkGray)
+            .padding(10.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+        ) {
+            HorizontalList(
+                categoryTitle = "Os mais populares",
+                movies
+            )
+            HorizontalList(
+                categoryTitle = "Grátis para assistir",
+                movies
+            )
+            HorizontalList(
+                categoryTitle = "Documentários",
+                movies
+            )
+            HorizontalList(
+                categoryTitle = "Aventura",
+                movies
+            )
+            HorizontalList(
+                categoryTitle = "Clássicos",
+                movies
+            )
+        }
+    }
+}
+
+@Composable
+fun HorizontalList(
+    categoryTitle: String,
+    movies: List<String>
+) {
+    //Add Loading
+    Text(
+        text = categoryTitle,
+        color = Color.White,
+        modifier = Modifier.padding(5.dp, bottom = 10.dp),
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    )
+    LazyRow {
+        items(movies) { movie ->
+            MovieCard(movie = movie)
+        }
+    }
+}
+
+@Composable
+fun MovieCard(movie: String) {
+    Card(
+        shape = RoundedCornerShape(5.dp),
+        contentColor = Color.Black,
+        elevation = 12.dp,
+        modifier = Modifier
+            .size(width = 125.dp, height = 175.dp)
+            .padding(5.dp, bottom = 30.dp)
     ) {
         Text(
-            text = "Home",
-            fontSize = 50.sp,
+            text = movie,
+            modifier = Modifier
+                .padding(10.dp),
         )
     }
 }
