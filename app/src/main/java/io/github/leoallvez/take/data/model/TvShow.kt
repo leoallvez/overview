428f9e9fe4baced30e44416e69c40a5d067c4504
leoallvez <leoallvez@gmail.com>
2022-04-26 00:20:15 -0300

    Movie and TvShow models with suggestion_id foreign.

--- app/src/main/java/io/github/leoallvez/take/data/model/TvShow.kt
+++ app/src/main/java/io/github/leoallvez/take/data/model/TvShow.kt
@@ -2,10 +2,22 @@ package io.github.leoallvez.take.data.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 
-@Entity(tableName = "tv_shows")
+
+@Entity(
+    tableName = "tv_shows",
+    foreignKeys = [
+        ForeignKey(
+            entity = Suggestion::class,
+            parentColumns = ["suggestion_id"],
+            childColumns =  ["suggestion_id"],
+            onDelete = ForeignKey.CASCADE,
+        )
+    ]
+)
 data class TvShow(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "tv_show_id")
@@ -20,7 +32,7 @@ data class TvShow(
     @field:Json(name = "vote_average")
     @ColumnInfo(name = "vote_average")
     override val voteAverage: Double,
-    @ColumnInfo(name = "suggestion_id")
+    @ColumnInfo(name = "suggestion_id", index = true)
     override var suggestionId: Long = 0,
 ) : Audiovisual {
     override fun getContentTitle(): String = name ?: "Name is null"
