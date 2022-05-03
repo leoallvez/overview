a1fc8afd69fae882b664e8f06d315fd9c67eb3e9
leoallvez <leoallvez@gmail.com>
2022-05-03 03:05:36 -0300

    improves home designer

--- app/src/main/java/io/github/leoallvez/take/ui/home/HomeScreen.kt
+++ app/src/main/java/io/github/leoallvez/take/ui/home/HomeScreen.kt
@@ -1,35 +1,39 @@
 package io.github.leoallvez.take.ui.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import io.github.leoallvez.take.R
-import io.github.leoallvez.take.ui.AdsBanner
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import io.github.leoallvez.take.R
 import io.github.leoallvez.take.data.model.AudioVisual
 import io.github.leoallvez.take.data.model.SuggestionResult
+import io.github.leoallvez.take.ui.AdsBanner
 import io.github.leoallvez.take.util.getStringByName
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
+
     val suggestions = viewModel.getSuggestions().observeAsState(listOf()).value
     val showAd = viewModel.adsAreVisible().observeAsState(initial = false)
 
@@ -44,7 +48,7 @@ fun HomeScreen(viewModel: HomeViewModel) {
         Column {
             AdsBanner(
                 bannerId = R.string.banner_sample_id,
-                isVisible = showAd.value
+                isVisible = false//showAd.value
             )
             Spacer(modifier = Modifier.padding(end = 10.dp))
             SuggestionVerticalList(
@@ -88,18 +92,18 @@ fun ListTitle(title: String) {
         text = title,
         color = Color(0xFFFFB400),
         modifier = Modifier
-            .padding(start = 5.dp, bottom = 5.dp, top = 20.dp),
+            .padding(start = 5.dp, bottom = 5.dp, top = 15.dp),
         fontSize = 20.sp,
         fontWeight = FontWeight.Bold,
-        //style = MaterialTheme.typography.body1,
     )
 }
-
+//TODO: refactor
 @Composable
 fun AudioVisualCard(audioVisual: AudioVisual) {
+    val with = 140.dp
     Column(
         modifier = Modifier
-            .size(width = 150.dp, height = 260.dp)
+            .size(width = 140.dp, height = 265.dp).padding(5.dp)
     ) {
         audioVisual.apply {
             AudioVisualImage(
@@ -107,11 +111,11 @@ fun AudioVisualCard(audioVisual: AudioVisual) {
                 contentDescription = getContentTitle(),
                 modifier = Modifier
                     .size(
-                        width = 150.dp,
+                        width = with,
                         height = 200.dp
-                    ).padding(5.dp),
+                    ).border(1.dp, color = Color.Yellow),
             )
-            AudioVisualTitle(title = getContentTitle())
+            AudioVisualTitle(title = getContentTitle(), size = with)
         }
     }
 }
@@ -126,7 +130,7 @@ fun AudioVisualImage(
         Card(
             shape = RoundedCornerShape(6.dp),
             contentColor = Color.Black,
-            elevation = 7.dp,
+            elevation = 15.dp,
         ) {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
@@ -142,13 +146,16 @@ fun AudioVisualImage(
 }
 
 @Composable
-fun AudioVisualTitle(title: String) {
+fun AudioVisualTitle(title: String, size: Dp) {
     Text(
         color = Color.White,
         text = title,
-        modifier = Modifier.padding(4.dp),
-        fontSize = 14.sp,
-        //fontWeight = FontWeight.Bold,
-        //style = MaterialTheme.typography.body1,
+        modifier = Modifier
+            .padding(top = 5.dp)
+            .size(size).border(1.dp, color = Color.Yellow),
+        fontSize = 13.sp,
+        maxLines = 3,
+        overflow = TextOverflow.Ellipsis,
+        textAlign = TextAlign.Center,
     )
 }
