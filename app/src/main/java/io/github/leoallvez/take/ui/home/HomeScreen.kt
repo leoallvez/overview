9dcbb1ee74a4d2c01538a21a67db78f03960f5bb
leoallvez <leoallvez@gmail.com>
2022-05-12 01:25:36 -0300

    testing collapsing toolbar

--- app/src/main/java/io/github/leoallvez/take/ui/home/HomeScreen.kt
+++ app/src/main/java/io/github/leoallvez/take/ui/home/HomeScreen.kt
@@ -1,21 +1,21 @@
 package io.github.leoallvez.take.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.leoallvez.take.R
 import io.github.leoallvez.take.data.model.AudioVisual
 import io.github.leoallvez.take.data.model.SuggestionResult
@@ -23,29 +23,57 @@ import io.github.leoallvez.take.ui.AdsBannerBottomAppBar
 import io.github.leoallvez.take.ui.HorizontalAudioVisualCard
 import io.github.leoallvez.take.ui.ListTitle
 import io.github.leoallvez.take.util.getStringByName
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
 
     val suggestions = viewModel.getSuggestions().observeAsState(listOf())
     val showAd = viewModel.adsAreVisible().observeAsState(initial = false)
+    val state = rememberCollapsingToolbarScaffoldState()
 
-    Scaffold(
-        topBar = {},
-        content = {
-            HomeScreenContent(suggestions.value)
-        },
-        bottomBar = {
-            AdsBannerBottomAppBar(
-                bannerId = R.string.banner_sample_id,
-                isVisible = showAd.value
+    CollapsingToolbarScaffold(
+        modifier = Modifier,
+        scrollStrategy = ScrollStrategy.EnterAlways,
+        state = rememberCollapsingToolbarScaffoldState(),
+        toolbar = {
+            val textSize = (18 + (30 - 18) * state.toolbarState.progress).sp
+
+            Image(
+                modifier = Modifier
+                    .parallax(ratio = 0.2f)
+                    .background(Color.Black)
+                    .fillMaxWidth()
+                    .height(245.dp)
+                    .pin(),
+                painter = painterResource(id = R.drawable.aranha),
+                contentDescription = null
             )
-        }
-    )
+
+            Text(
+                text = "Take",
+                modifier = Modifier
+                    .road(Alignment.CenterStart, Alignment.BottomEnd)
+                    .padding(10.dp, 16.dp, 16.dp, 16.dp),
+                color = Color.White,
+                fontSize = textSize
+            )
+        },
+    ) {
+        HomeScreenContent(
+            suggestions = suggestions.value,
+            adsBannerIsVisible = showAd.value
+        )
+    }
 }
 
 @Composable
-fun HomeScreenContent(suggestions: List<SuggestionResult>) {
+fun HomeScreenContent(
+    suggestions: List<SuggestionResult>,
+    adsBannerIsVisible: Boolean
+) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
@@ -55,8 +83,12 @@ fun HomeScreenContent(suggestions: List<SuggestionResult>) {
             .padding(10.dp, bottom = 50.dp),
     ) {
         Column {
+            AdsBannerBottomAppBar(
+                bannerId = R.string.banner_sample_id,
+                isVisible = adsBannerIsVisible
+            )
             SuggestionVerticalList(
-                suggestions
+                suggestions = suggestions
             )
         }
     }
@@ -64,11 +96,11 @@ fun HomeScreenContent(suggestions: List<SuggestionResult>) {
 
 @Composable
 fun SuggestionVerticalList(
-    moviesSuggestions: List<SuggestionResult>
+    suggestions: List<SuggestionResult>
 ) {
     val context = LocalContext.current
     LazyColumn {
-        items(moviesSuggestions) {
+        items(suggestions) {
             AudioVisualHorizontalList(
                 title = context.getStringByName(it.titleResourceId),
                 audioVisuals = it.audioVisuals
