5a7f1e1f4e1c1f86e9ca8b47089488c3fe9d8e0e
leoallvez <leoallvez@hotmail.com>
2022-06-28 19:39:17 -0300

    indentation adjustment in the Screen class

--- app/src/main/java/io/github/leoallvez/take/ui/AppNavigation.kt
+++ app/src/main/java/io/github/leoallvez/take/ui/AppNavigation.kt
@@ -1,11 +1,11 @@
 package io.github.leoallvez.take.ui
 
 sealed class Screen(val route: String, val name: String, val paramName: String = "") {
-    object Splash: Screen(route = "splash_screen", name = "SplashScreen")
+    object Splash : Screen(route = "splash_screen", name = "SplashScreen")
 
-    object Home: Screen(route = "home_screen", name = "HomeScreen")
+    object Home : Screen(route = "home_screen", name = "HomeScreen")
 
-    object MediaDetails: Screen(
+    object MediaDetails : Screen(
         route = "media_detail/{media_id}",
         name = "MediaDetail",
         paramName = "media_id"
