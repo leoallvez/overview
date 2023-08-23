e6ef421c678810fd86019f5ff9f42074a3219d82
leoallvez <leoallvez@gmail.com>
2023-08-22 21:58:25 -0300

    Trabalhando na implementação de media cache - melhoria no repositório;

--- app/src/main/java/br/com/deepbyte/overview/data/repository/media/MediaCacheRepository.kt
+++ app/src/main/java/br/com/deepbyte/overview/data/repository/media/MediaCacheRepository.kt
@@ -1,5 +1,6 @@
 package br.com.deepbyte.overview.data.repository.media
 
+import br.com.deepbyte.overview.data.model.media.MediaEntity
 import br.com.deepbyte.overview.data.repository.media.interfaces.IMediaCacheRepository
 import br.com.deepbyte.overview.data.sampe.mediaEntitySamples
 import br.com.deepbyte.overview.data.source.media.local.suggestion.MediaLocalDataSource
@@ -8,26 +9,28 @@ import javax.inject.Inject
 class MediaCacheRepository @Inject constructor(
     private val _sourceLocal: MediaLocalDataSource
 ) : IMediaCacheRepository {
+
     override fun saveCache(): Boolean = with(_sourceLocal) {
         val newMedias = mediaEntitySamples
         return@with if (newMedias.isNotEmpty()) {
-            val likedMedias = getLiked()
             deleteNotLiked()
-
-            newMedias.forEach { newMedia ->
-                with(newMedia) {
-                    val likedMedia = likedMedias.find { it.apiId == newMedia.apiId }
-                    likedMedia?.let {
-                        isLiked = true
-                        dbId = likedMedia.dbId
-                    }
-                    isIndicated = true
-                }
-            }
+            updateAttributes(newMedias)
             insert(newMedias)
             true
         } else {
             false
+        }
+    }
+
+    private fun updateAttributes(newMedias: List<MediaEntity>) {
+        val likedMedias = _sourceLocal.getLiked()
+        newMedias.forEach { newMedia ->
+            val likedMedia = likedMedias.find { it.apiId == newMedia.apiId }
+            likedMedia?.let {
+                newMedia.isLiked = true
+                newMedia.dbId = it.dbId
+            }
+            newMedia.isIndicated = true
         }
     }
 }
