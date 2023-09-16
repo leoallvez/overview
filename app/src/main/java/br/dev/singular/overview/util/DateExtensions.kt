9d49392eb58ae485a4827ef6d47e0f6a493982f5
leoallvez <leoallvez@gmail.com>
2023-09-15 22:55:47 -0300

    tratamento de lint report;

--- app/src/main/java/br/dev/singular/overview/util/DateExtensions.kt
+++ app/src/main/java/br/dev/singular/overview/util/DateExtensions.kt
@@ -3,10 +3,12 @@ package br.dev.singular.overview.util
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
 const val API_DATE_PATTERN = "yyyy-MM-dd"
 
-fun Date.toFormatted(): String = SimpleDateFormat(API_DATE_PATTERN).format(this)
+fun Date.toFormatted(): String =
+    SimpleDateFormat(API_DATE_PATTERN, Locale.getDefault()).format(this)
 
 // write an extension method that takes a Date back one month
 fun Date.toLastMonthFormatted(): String {
