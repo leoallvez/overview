b816338ab568e8c60dd4f1cee435c59d57a877fa
leoallvez <leoallvez@gmail.com>
2023-09-30 04:00:21 -0300

    upgrade compose ui-test;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -181,6 +181,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0-alpha04")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0-alpha06")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
