14ba0fdfc4f2c4bae3ebee0866b3b7acf002d298
leoallvez <leoallvez@hotmail.com>
2023-12-19 23:56:11 -0300

    fix: SplashScreen.kt lint
    Signed-off-by: leoallvez <leoallvez@hotmail.com>

--- app/src/main/java/br/dev/singular/overview/ui/splash/SplashScreen.kt
+++ app/src/main/java/br/dev/singular/overview/ui/splash/SplashScreen.kt
@@ -28,7 +28,6 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(onOpen: () -> Unit, viewModel: SplashViewModel = hiltViewModel()) {
-
     TrackScreenView(screen = ScreenNav.Splash, tracker = viewModel.analyticsTracker)
 
     val scale = remember { Animatable(0f) }
