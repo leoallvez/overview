0de9c96411a51c2659358a498cafcedf782b60b8
Léo Alves <leoallvez@hotmail.com>
2025-02-08 19:00:21 -0300

    chore(analytics): try fix lint

--- app/src/main/java/br/dev/singular/overview/ui/tagging/TaggingHelper.kt
+++ app/src/main/java/br/dev/singular/overview/ui/tagging/TaggingHelper.kt
@@ -7,7 +7,6 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import timber.log.Timber
 import javax.inject.Inject
 
-
 interface ITaggingHelper {
     fun logScreenView(customPath: String)
     fun logClick(customPath: String, detail: String)
@@ -18,16 +17,20 @@ class TaggingHelper @Inject constructor(
 ) : ITaggingHelper {
 
     override fun logScreenView(customPath: String) {
-        logEvent(TaggingEvents.SCREEN_VIEW, Bundle().apply {
-            putString(TaggingParams.CUSTOM_PATH, customPath)
-        })
+        logEvent(
+            TaggingEvents.SCREEN_VIEW,
+            Bundle().apply { putString(TaggingParams.CUSTOM_PATH, customPath) }
+        )
     }
 
     override fun logClick(customPath: String, detail: String) {
-        logEvent(TaggingEvents.CLICK, Bundle().apply {
-            putString(TaggingParams.CUSTOM_PATH, customPath)
-            putString(TaggingParams.DETAIL, detail)
-        })
+        logEvent(
+            TaggingEvents.CLICK,
+            Bundle().apply {
+                putString(TaggingParams.CUSTOM_PATH, customPath)
+                putString(TaggingParams.DETAIL, detail)
+            }
+        )
     }
 
     private fun logEvent(name: String, bundle: Bundle) {
