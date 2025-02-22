c8ef4b03733eedc9dc26a395b2558d335a92b40d
Léo Alves <leoallvez@hotmail.com>
2025-02-22 17:16:19 -0300

    fix(suggestion): lint

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -9,6 +9,7 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     languageVersion = "1.9"
