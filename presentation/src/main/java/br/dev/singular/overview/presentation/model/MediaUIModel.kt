43c837f4852e227bf60803bb8dae56c53c67f317
Léo Alves <leoallvez@hotmail.com>
2025-04-07 21:54:53 -0300

    feat(ux): section title composable

--- presentation/src/main/java/br/dev/singular/overview/presentation/model/MediaUIModel.kt
+++ presentation/src/main/java/br/dev/singular/overview/presentation/model/MediaUIModel.kt
@@ -3,6 +3,6 @@ package br.dev.singular.overview.presentation.model
 class MediaUIModel(
     val id: Long,
     val type: String,
-    val title: String,
+    val letter: String,
     val posterPath: String
 )
