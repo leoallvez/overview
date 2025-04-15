4e0cdc5481a8702f0dc454a49b629341a843e610
Léo Alves <leoallvez@hotmail.com>
2025-04-14 22:17:57 -0300

    feat(compose): improving MediaUIModel attributes names

--- presentation/src/main/java/br/dev/singular/overview/presentation/ui/MediaPoster.kt
+++ presentation/src/main/java/br/dev/singular/overview/presentation/ui/MediaPoster.kt
@@ -4,15 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.dev.singular.overview.presentation.R
 import br.dev.singular.overview.presentation.model.MediaUIModel
 import br.dev.singular.overview.presentation.ui.theme.PrimaryBackground
 
@@ -24,13 +21,13 @@ fun MediaPoster(media: MediaUIModel, onClick: () -> Unit) {
             .clickable { onClick.invoke() }
     ) {
         BasicImage(
-            url = media.posterPath,
-            contentDescription = media.letter,
+            url = media.posterURLPath,
+            contentDescription = media.title,
             withBorder = true,
-            modifier = Modifier.height(200.dp)
+            modifier = Modifier.height(200.dp).width(130.dp)
         )
         BasicText(
-            text = media.letter,
+            text = media.title,
             modifier = Modifier.width(130.dp),
             style = MaterialTheme.typography.bodySmall,
             isBold = true
@@ -44,8 +41,8 @@ fun MediaPosterPreview() {
     MediaPoster(
         MediaUIModel(
             id = 1,
-            letter = "Matrix",
-            posterPath = "https://imagens.com/movie.jpg",
+            title = "Matrix",
+            posterURLPath = "https://imagens.com/movie.jpg",
             type = "Movie"
         )
     ) { }
