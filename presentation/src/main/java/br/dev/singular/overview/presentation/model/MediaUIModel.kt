282df04ea79f9fc808fe144b9d36bdb064bf345f
Léo Alves <leoallvez@hotmail.com>
2025-04-14 22:17:57 -0300

    feat(compose): improving MediaUIModel attributes names

--- presentation/src/main/java/br/dev/singular/overview/presentation/model/MediaUIModel.kt
+++ presentation/src/main/java/br/dev/singular/overview/presentation/model/MediaUIModel.kt
@@ -3,6 +3,6 @@ package br.dev.singular.overview.presentation.model
 class MediaUIModel(
     val id: Long,
     val type: String,
-    val letter: String,
-    val posterPath: String
+    val title: String,
+    val posterURLPath: String
 )
