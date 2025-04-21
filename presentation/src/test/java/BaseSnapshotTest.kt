b823dc3cf085bbcad89e77916247512e5f497038
Léo Alves <leoallvez@hotmail.com>
2025-04-20 21:01:20 -0300

    test(snapshot): basic text

--- presentation/src/test/java/BaseSnapshotTest.kt
+++ presentation/src/test/java/BaseSnapshotTest.kt
@@ -1,5 +1,11 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
+import br.dev.singular.overview.presentation.ui.theme.PrimaryBackground
 import org.junit.Rule
 
 abstract class BaseSnapshotTest() {
@@ -9,4 +15,11 @@ abstract class BaseSnapshotTest() {
         deviceConfig = DeviceConfig.PIXEL_5
     )
 
+    protected fun snapshot(sutComposable: @Composable () -> Unit) {
+        paparazzi.snapshot {
+            Box(modifier = Modifier.fillMaxSize().background(PrimaryBackground)) {
+                sutComposable()
+            }
+        }
+    }
 }
