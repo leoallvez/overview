c817a2428fc3f7dc7d22e7d994c518e9f236f28f
Léo Alves <leoallvez@hotmail.com>
2025-04-20 21:01:20 -0300

    test(snapshot): basic text

--- presentation/src/test/java/br/dev/singular/overview/presentation/ui/text/BasicTextSnapshotTest.kt
+++ presentation/src/test/java/br/dev/singular/overview/presentation/ui/text/BasicTextSnapshotTest.kt
@@ -1,30 +1,44 @@
 package br.dev.singular.overview.presentation.ui.text
 
 import BaseSnapshotTest
-import android.R
+import androidx.compose.foundation.background
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle.Companion.Italic
 import org.junit.Test
 
 class BasicTextSnapshotTest : BaseSnapshotTest() {
 
     @Test
-    fun filled() {
-        paparazzi.snapshot {
-            BasicText(text = "Lorem ipsum dolor sit amet")
-        }
+    fun filled() = snapshot {
+        BasicText(text = "Lorem ipsum dolor sit amet")
     }
 
     @Test
-    fun isBold() {
-        paparazzi.snapshot {
-            BasicText(text = "Lorem ipsum dolor sit amet", isBold = true)
-        }
+    fun isBold() = snapshot {
+        BasicText(text = "Lorem ipsum dolor sit amet", isBold = true)
     }
 
     @Test
-    fun changeColor() {
-        paparazzi.snapshot {
-            BasicText(text = "Lorem ipsum dolor sit amet", color = Color.Yellow)
-        }
+    fun changeColor() = snapshot {
+        BasicText(text = "Lorem ipsum dolor sit amet", color = Color.Yellow)
+    }
+
+    @Test
+    fun withModifier() = snapshot {
+        BasicText(
+            text = "Lorem ipsum dolor sit amet",
+            color = Color.Black,
+            modifier = Modifier.background(Color.White)
+        )
+    }
+
+    @Test
+    fun withStyle() = snapshot {
+        BasicText(
+            text = "Lorem ipsum dolor sit amet",
+            style = TextStyle(fontStyle = Italic)
+        )
     }
 }
