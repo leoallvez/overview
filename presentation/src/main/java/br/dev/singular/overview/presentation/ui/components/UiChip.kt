18915823695e14241dbbd40be1d2167d1d47c816
Léo Alves <leoallvez@hotmail.com>
2025-09-20 02:04:25 -0300

    feat: creating the chip and icon components

---
+++ presentation/src/main/java/br/dev/singular/overview/presentation/ui/components/UiChip.kt
@@ -0,0 +1,79 @@
+package br.dev.singular.overview.presentation.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.dev.singular.overview.presentation.R
+import br.dev.singular.overview.presentation.ui.components.text.UiText
+import br.dev.singular.overview.presentation.ui.theme.HighlightColor
+import br.dev.singular.overview.presentation.ui.theme.LowlightColor
+import br.dev.singular.overview.presentation.ui.theme.Surface
+
+@Composable
+fun UiChip(
+    text: String,
+    modifier: Modifier = Modifier,
+    activated: Boolean = false,
+    icon: @Composable (() -> Unit) = {},
+    onClick: () -> Unit
+) {
+    val color = if (activated) HighlightColor else LowlightColor
+    FilterChip(
+        onClick = onClick,
+        modifier = modifier.height(30.dp),
+        label = {
+            UiText(text, isBold = activated, color = color)
+        },
+        colors = FilterChipDefaults.filterChipColors(
+            selectedContainerColor = Surface,
+        ),
+        selected = activated,
+        shape = RoundedCornerShape(percent = 50),
+        trailingIcon = icon,
+        border = BorderStroke(dimensionResource(R.dimen.border_width), color),
+    )
+}
+
+@Preview(name = "Activated")
+@Composable
+internal fun UiChipActivatedPreview() {
+    UiChipPreviewHelper(true)
+}
+
+@Preview(name = "Not activated")
+@Composable
+internal fun UiChipNotActivatedPreview() {
+    UiChipPreviewHelper(false)
+}
+
+@Composable
+private fun UiChipPreviewHelper(initialState: Boolean) {
+    var activated by rememberSaveable { mutableStateOf(initialState) }
+    UiChip(
+        text = "Label",
+        activated = activated,
+        icon = {
+            if (activated) {
+                UiIcon(
+                    icon = Icons.Filled.Clear,
+                    color = HighlightColor
+                )
+            }
+        }
+    ) {
+        activated = !activated
+    }
+}
---
+++ presentation/src/main/java/br/dev/singular/overview/presentation/ui/components/UiIcon.kt
@@ -0,0 +1,51 @@
+package br.dev.singular.overview.presentation.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.sharp.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import br.dev.singular.overview.presentation.R
+import br.dev.singular.overview.presentation.ui.theme.HighlightColor
+import br.dev.singular.overview.presentation.ui.theme.WarningColor
+
+@Composable
+fun UiIcon(icon: ImageVector, color: Color = HighlightColor) {
+    Icon(
+        tint = color,
+        imageVector = icon,
+        contentDescription = null,
+        modifier = Modifier.size(dimensionResource(R.dimen.spacing_large))
+    )
+}
+
+@Composable
+fun UiIcon(icon: Painter, color: Color = HighlightColor) {
+    Icon(
+        tint = color,
+        painter = icon,
+        contentDescription = null,
+        modifier = Modifier.size(dimensionResource(R.dimen.spacing_large))
+    )
+}
+
+@Preview
+@Composable
+internal fun UiIconPreview() {
+    Column {
+        UiIcon(icon = Icons.Filled.Close)
+        UiIcon(icon = painterResource(id = R.drawable.ic_outline_alert))
+        UiIcon(icon = Icons.Filled.KeyboardArrowDown)
+        UiIcon(icon = Icons.Sharp.FavoriteBorder, color = WarningColor)
+    }
+}
